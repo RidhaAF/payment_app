bf8ea6a12b5683605edcaabc1346150cadf9103b
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-06-17 14:13:52 +0700

    Add Header

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payment_app/theme.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,12 +11,50 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: Column(
-          children: [
-            Image.asset(
-              'assets/icon_one.png',
-            ),
-          ],
+        backgroundColor: backgroundColor,
+        body: Padding(
+          padding: EdgeInsets.only(
+            top: 50.0,
+            left: defaultMargin,
+            right: defaultMargin,
+          ),
+          child: Column(
+            children: [
+              Center(
+                child: Image.asset(
+                  'assets/icon_one.png',
+                  height: 200,
+                  width: 267,
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Upgrade to ',
+                    style: titleTextStyle,
+                  ),
+                  Text(
+                    'Pro',
+                    style: titleTextStyle.copyWith(
+                      color: Color(0XFF007DFF),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                'Go unlock all features and\nbuild your own business bigger',
+                style: subtitleTextStyle,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+double defaultMargin = 32.0;
+
 Color whiteColor = Colors.white;
 Color subtitleColor = Color(0XFF8997B8);
+Color backgroundColor = Color(0XFF04112F);
 
 TextStyle titleTextStyle = GoogleFonts.poppins(
   color: whiteColor,
