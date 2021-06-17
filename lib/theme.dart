72115f8de77968f5bf580f23df1ed6b0c2601719
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-06-17 13:53:36 +0700

    Add Theme

---
+++ lib/theme.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color whiteColor = Colors.white;
+Color subtitleColor = Color(0XFF8997B8);
+
+TextStyle titleTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontWeight: FontWeight.bold,
+  fontSize: 26,
+);
+
+TextStyle subtitleTextStyle = GoogleFonts.poppins(
+  color: subtitleColor,
+  fontWeight: FontWeight.w300,
+  fontSize: 16,
+);
+
+TextStyle planTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 14,
+);
+
+TextStyle optionTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontWeight: FontWeight.w300,
+  fontSize: 12,
+);
+
+TextStyle priceTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontWeight: FontWeight.bold,
+  fontSize: 18,
+);
+
+TextStyle buttonTextStyle = GoogleFonts.poppins(
+  color: whiteColor,
+  fontWeight: FontWeight.w600,
+  fontSize: 16,
+);
